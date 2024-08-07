a7237245525d0754ecc6905d49d2127b5eb64586
刘俊峰 <329960622@qq.com>
2024-08-08 00:14:28 +0800

    登录背景提交

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 import { Space, Input, Button } from "antd";
 import styles from './login.module.scss'
-// import initLoginBg from './init.ts'//会报错（红色下划线）需要在vite-env.d.ts声明
+import initLoginBg from './init.ts'//会报错（红色下划线）需要在vite-env.d.ts声明
 const Login = () => {
     //生命周期，，加载完这个函数组件之后，执行
     // passwordVisible
     // const [passwordVisible,setPasswordVisible]= useState('Hide')
+    useEffect(()=>{
+        initLoginBg()
+        window.onresize = function(){initLoginBg()};
+    },[])
+    
     return (
         <div className={styles.loginPage}>
+            <canvas id="canvas" style={{display:"block"}}></canvas>
             <div className={styles.loginBox}>
                 <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
                     <h1>用户登录</h1>
